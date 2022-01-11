71d7d5602ce682751bf8e321dd923b951313442b
feiyoung <1029725413@qq.com>
2022-01-11 22:12:55 +0800

    Update MIG_real_exmaple.m

--- MIG_real_exmaple.m
+++ MIG_real_exmaple.m
@@ -1,7 +1,7 @@
 type = cell(1,2);
 type{1,1} = 'binomial';  type{1,2} = 'probit'; 
 group = ones(1, size(Xmis,2));
-q= 3; lambda=100; verbose=0;
+q= 3; 
 % imputation using MIG
 tic; 
 [hX, hD, hHm, hBm] = MIG(Xmis, group, type, q);
